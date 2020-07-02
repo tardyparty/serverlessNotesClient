635771895ee23d87e778ac58630b46b6b85181c7
tardyparty <davidtardy@MacBook-Pro.local>
2020-07-02 16:45:54 -0500

    404 routing page handled

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
     return (
         <Switch>
+
+            {/* Send here first */}
             <Route exact path="/">
                 <Home />
+            </Route>
+
+            {/* Catch all unmatched routes */}
+            <Route>
+                <NotFound />
             </Route>
         </Switch>
     );
