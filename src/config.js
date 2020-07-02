2ad94c504e3e70db52d0138e7a58b0d3ea485037
tardyparty <davidtardy@MacBook-Pro.local>
2020-07-02 16:57:37 -0500

    aws amplify configured for client side

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "tard-notes-2"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://yepa569207.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_MZl26TN9h",
+        APP_CLIENT_ID: "2ca6nvna13lt9uo86ghuakmmsr",
+        IDENTITY_POOL_ID: "us-east-2:81c738e1-dc70-479a-b837-fdc0b19ce9a5",
+    }
+};
