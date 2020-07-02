b6d55e043448a3d887ecc1ec68a8510c28fd7f07
tardyparty <davidtardy@MacBook-Pro.local>
2020-07-02 18:17:32 -0500

    custom hook made for handling forms

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import { Auth } from "aws-amplify";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
-import { useAppContext } from "../libs/contextLib";
 import { useHistory } from "react-router-dom";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../libs/contextLib";
+import { useFormFields } from "../libs/hooksLib";
 import { onError } from "../libs/errorLib";
+import "./Login.css";
 
 
 export default function Login() {
-    const [isLoading, setIsLoading] = useState(false);
-    const { userHasAuthenticated } = useAppContext();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const history = useHistory();
+    const { userHasAuthenticated } = useAppContext();
+    const [isLoading, setIsLoading] = useState(false);
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+    });
+    
 
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
 
     async function handleSubmit(event) {
@@ -26,7 +30,7 @@ export default function Login() {
         setIsLoading(true);
 
         try {
-            await Auth.signIn(email, password);
+            await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
             history.push("/");
         } catch (e) {
@@ -45,8 +49,8 @@ export default function Login() {
                     <FormControl
                         autofocus
                         type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        value={fields.email}
+                        onChange={handleFieldChange}
                     />
                 </FormGroup>
 
@@ -54,8 +58,8 @@ export default function Login() {
                 <FormGroup controlId="password" bsSize="large">
                     <ControlLabel>Password</ControlLabel>
                     <FormControl
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        value={fields.password}
+                        onChange={handleFieldChange}
                         type="password"
                     />
                 </FormGroup>
